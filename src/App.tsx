3708f3f5f0b53c1223f80fbccfd8e3ae1faa7c8b
Harish <harish2n5@gmail.com>
2025-07-06 18:36:23 +0530

    tc

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 
 // const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 const queryClient = new QueryClient();
 
 const App = () => (
